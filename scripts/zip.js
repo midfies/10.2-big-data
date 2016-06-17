c59b88e4064729d1a7509f88efa34842460625e3
Rick Patci <rpatci@gmail.com>
2016-06-17 11:14:03 -0700

    push big data lecture code

--- scripts/zip.js
+++ scripts/zip.js
@@ -5,13 +5,56 @@
     }
   };
 
-  
+  var neighborhoodTotals = {};
+  Zip.topFive = [];
 
-  Zip.fetchAll = function() {
+  getData = function() {
     $.getJSON('/data/manhattan.json', function(data) {
-
+      Zip.allZips = data.features.map(returnPropsObj);
+      Zip.allZips.forEach(theCount);
+      populateTotals(neighborhoodTotals);
     });
   };
 
+  // Initial population of properties from server data
+  function returnPropsObj(obj) {
+    return {
+      zipCode: obj.properties.zip,
+      address: !!obj.properties.address ? obj.properties.address : undefined,
+      neighborhood: !!obj.properties.neighborhood ? obj.properties.neighborhood.split(', ').splice(0, 1).join() : undefined,
+      coordinates: {
+        lat: obj.geometry.coordinates[1],
+        lng: obj.geometry.coordinates[0]
+      }
+    }
+  }
+
+  // Create a running tab of neighborhood counts
+  function theCount(zipObj) {
+    if (!neighborhoodTotals[zipObj.neighborhood]) {
+      neighborhoodTotals[zipObj.neighborhood] = 1;
+    } else neighborhoodTotals[zipObj.neighborhood] ++;
+  }
+
+  // Order the results above based on neighborhood frequency
+  function populateTotals(obj) {
+    for (neighborhood in obj) {
+      Zip.topFive.push([neighborhood, obj[neighborhood]]);
+      Zip.topFive.sort(highest).splice(5);
+    }
+    Zip.topFive = Zip.topFive.reduce(statsFriendly, {});
+  }
+
+  // Transform 'topFive' array of arrays into a readable object
+  function statsFriendly(statsObj, curArray) {
+    statsObj[curArray[0]] = curArray[1];
+    return statsObj;
+  }
+
+  function highest(currentneighborhood, nextNeighborhood) {
+    return nextNeighborhood[1] - currentneighborhood[1];
+  }
+
+  getData();
   module.Zip = Zip;
 })(window);
