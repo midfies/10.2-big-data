5fa32d8f7104019d8a36d65b24fb1883a2c696eb
Rick Patci <rpatci@gmail.com>
2016-08-26 08:19:03 -0700

    weekend fun

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
-### Big Data
+### Big Data with geoJSON
 
-#### Use live-server to open this project up.
+The goal for this assignment is to simulate a data set that you may
+encounter during project week.
 
- - In the console 
+Let's focus only on transforming this collection. No need to
+invoke a constructor, or pass off control to the View.
+
+Start simple:
+
+Transform the initial collection into
+an array of each object's properties.
+
+We first want to pick up on only a few properties, therefore, you may need to build these objects out dynamically with just the following properties:
+
+The zip code
+Its neighborhood
+Its address (if applicable, otherwise null)
+Ensure that this new collection also contains the coordinates for each zip. The "coordinates" property should be its own object with "lat" and "lng" properties since we'd like to incorporate Google Maps later.
+
+Finally, populate a top five array of neighborhood objects (in descending order) based on the number of zips they associate with! Each object should have two properties: the neighborhood, and the total number of zip codes. For example, the first element in the top five array should be an object with a "neighborhood" property equal to Midtown, and a "total" property equal to 37.
--- scripts/zip.js
+++ scripts/zip.js
@@ -1,48 +1,11 @@
 (function(module) {
   var zip = {};
-  neighborhoodTotals = {};
-  zip.topFive = [];
 
   getData = function() {
-    function justTheGoodProps(obj) {
-      return {
-        zipCode: obj.properties.zip,
-        neighborhood: !!obj.properties.neighborhood ? obj.properties.neighborhood.split(', ')[0] : null,
-        address: !!obj.properties.address ? obj.properties.address : null,
-        coordinates: {
-          lat: obj.geometry.coordinates[1],
-          lng: obj.geometry.coordinates[0]
-        }
-      }
-    }
-
     $.getJSON('/data/manhattan.json', function(data) {
-      data.features.map(justTheGoodProps)
-      .forEach(buildZipsDictionary);
-      generateTopFive(neighborhoodTotals);
+      // TODO: start here!
     });
   };
-
-  function buildZipsDictionary(zipObj) {
-    curNeighb = zipObj.neighborhood;
-    neighborhoodTotals[curNeighb] ?
-      neighborhoodTotals[curNeighb] ++ :
-      neighborhoodTotals[curNeighb] = 1;
-  }
-
-function generateTopFive(obj) {
-  for (neighborhood in obj) {
-    zip.topFive.push({
-      neighborhood: neighborhood,
-      totalZips: obj[neighborhood]
-    });
-  }
-  zip.topFive.sort(descending).splice(5);
-}
-
-descending = function(curNeighb, nextNeighb) {
-  return nextNeighb.totalZips - curNeighb.totalZips;
-}
 
   getData();
   module.zip = zip;
