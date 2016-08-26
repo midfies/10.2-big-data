f856f7472352e76a6debb0a83a9f31892dca065e
Rick Patci <rpatci@gmail.com>
2016-08-26 07:52:34 -0700

    demo

--- scripts/zip.js
+++ scripts/zip.js
@@ -1,11 +1,48 @@
 (function(module) {
   var zip = {};
-  
+  neighborhoodTotals = {};
+  zip.topFive = [];
+
   getData = function() {
+    function justTheGoodProps(obj) {
+      return {
+        zipCode: obj.properties.zip,
+        neighborhood: !!obj.properties.neighborhood ? obj.properties.neighborhood.split(', ')[0] : null,
+        address: !!obj.properties.address ? obj.properties.address : null,
+        coordinates: {
+          lat: obj.geometry.coordinates[1],
+          lng: obj.geometry.coordinates[0]
+        }
+      }
+    }
+
     $.getJSON('/data/manhattan.json', function(data) {
-      // TODO: start here!
+      data.features.map(justTheGoodProps)
+      .forEach(buildZipsDictionary);
+      generateTopFive(neighborhoodTotals);
     });
   };
+
+  function buildZipsDictionary(zipObj) {
+    curNeighb = zipObj.neighborhood;
+    neighborhoodTotals[curNeighb] ?
+      neighborhoodTotals[curNeighb] ++ :
+      neighborhoodTotals[curNeighb] = 1;
+  }
+
+function generateTopFive(obj) {
+  for (neighborhood in obj) {
+    zip.topFive.push({
+      neighborhood: neighborhood,
+      totalZips: obj[neighborhood]
+    });
+  }
+  zip.topFive.sort(descending).splice(5);
+}
+
+descending = function(curNeighb, nextNeighb) {
+  return nextNeighb.totalZips - curNeighb.totalZips;
+}
 
   getData();
   module.zip = zip;
